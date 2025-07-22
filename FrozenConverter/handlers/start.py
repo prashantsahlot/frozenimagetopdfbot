f5e87bcc05922f2c8dfb90b5ec29e43b99e33f34
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2025-07-22 11:59:52 +0530

    Update start.py

--- FrozenConverter/handlers/start.py
+++ FrozenConverter/handlers/start.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+def register_start(app: Client):
+    @app.on_message(filters.command("start"))
+    async def start_handler(_, message: Message):
+        await message.reply("👋 Send me images one by one.\nThen type /convert to get your PDF.")
 
